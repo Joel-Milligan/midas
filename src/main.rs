45c79a18f549ad01c734b7f80e1308ee8470ce77
Joel Milligan <joelmilligan24@gmail.com>
2026-01-16 19:26:22 +0800

    Clean up simulation and make it last longer

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use midas::{Game, Player, RoundResult};
 fn main() {
     for cutoff in 0..=21 {
         let mut total_lasted = 0;
-        for _ in 0..1000 {
+        for _ in 0..10_000 {
             let mut num_rounds = 0;
             let player = Player::new(cutoff);
             let mut game = Game::new(player);
@@ -24,23 +24,27 @@ fn main() {
             }
 
             total_lasted += num_rounds;
-
-            let wins = *results.get(&RoundResult::Blackjack).unwrap()
-                + *results.get(&RoundResult::Win).unwrap();
-            let win_percent = (wins as f32 / num_rounds as f32) * 100.;
-            let draws = *results.get(&RoundResult::Push).unwrap();
-            let draw_percent = (draws as f32 / num_rounds as f32) * 100.;
-            let losses = *results.get(&RoundResult::Lose).unwrap()
-                + *results.get(&RoundResult::Bust).unwrap();
-            let loss_percent = (losses as f32 / num_rounds as f32) * 100.;
-            println!(
-                "\t{num_rounds} rounds:\t{win_percent:.0}(W)\t{draw_percent:.0}(D)\t{loss_percent:.0}(L)",
-            );
+            // print_round_results(results, num_rounds);
         }
         println!(
-            "Cutoff of {:0>2} lasted an average of {} rounds.",
+            "Cutoff of {: >2} lasted an average of {} rounds.",
             cutoff,
-            total_lasted / 1000
+            total_lasted / 10_000
         );
     }
+}
+
+#[allow(dead_code)]
+fn print_round_results(results: HashMap<RoundResult, i32>, num_rounds: i32) {
+    let wins =
+        *results.get(&RoundResult::Blackjack).unwrap() + *results.get(&RoundResult::Win).unwrap();
+    let win_percent = (wins as f32 / num_rounds as f32) * 100.;
+    let draws = *results.get(&RoundResult::Push).unwrap();
+    let draw_percent = (draws as f32 / num_rounds as f32) * 100.;
+    let losses =
+        *results.get(&RoundResult::Lose).unwrap() + *results.get(&RoundResult::Bust).unwrap();
+    let loss_percent = (losses as f32 / num_rounds as f32) * 100.;
+    println!(
+        "\t{num_rounds} rounds:\t{win_percent:.0}(W)\t{draw_percent:.0}(D)\t{loss_percent:.0}(L)",
+    );
 }
